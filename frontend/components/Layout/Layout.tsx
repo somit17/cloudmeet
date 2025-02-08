5901126c7f37060a5111c7856a9c0142a033bfab
Somit Meshram <somitmeshram3256@gmail.com>
2025-02-08 15:13:27 +0530

    Add Basic Pages

--- frontend/components/Layout/Layout.tsx
+++ frontend/components/Layout/Layout.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Sidebar from './Sidebar/Sidebar'
+import MeetingPage from './Meetings/MeetingPage.'
+import WebcamComponent from './Webcam/Webcam'
+import VideoComponent from './VC/VC'
 
-function Layout() {
+const Layout = () => {
+  const meetingCount = Array(4).fill(null);
   return (
-    <div><Navbar/>
-    <Sidebar/></div>
+    <div><Navbar />
+      <Sidebar />
+      <VideoComponent />
+      <div className="grid">
+        {meetingCount.map((_, index) => (
+          <div key={index} className="col-4">
+            <MeetingPage meetingTitle={index.toString()} time="1:00 pm - 2:00 pm" />
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,6 +4,7 @@ import "primereact/resources/themes/lara-light-cyan/theme.css";
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
+import 'font-awesome/css/font-awesome.min.css';
 import Registration from '../components/Registration';
 import { GlobalProvider } from '../contextAPI/GlobalContext';
 import Layout from '../components/Layout/Layout';
